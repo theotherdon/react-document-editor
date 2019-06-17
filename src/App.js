3f010cfab63f2b5f217cd3bc61d31f2cec732a48
Don Schrimsher <don4thirteen@gmail.com>
2019-06-17 12:18:54 -0400

    Change to document structure
    Changes the app from a TODO list structure to a document structure that
    includes a title and content for each document.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import _ from "lodash"
+import faker from "faker"
 import List from "@material-ui/core/List"
 import ListItem from "@material-ui/core/ListItem"
 import ListItemText from "@material-ui/core/ListItemText"
-import Typography from "@material-ui/core/Typography"
 import Grid from "@material-ui/core/Grid"
 import TextField from "@material-ui/core/TextField"
 
@@ -11,52 +11,64 @@ import "./App.css"
 
 function App() {
   const [listItems, setListItems] = useState([
-    { id: 1, text: "Testing, 1, 2, 3" },
-    { id: 2, text: "Testing, 4, 5, 6" }
+    buildSampleDocument(),
+    buildSampleDocument()
   ])
   const [selectedListItem, setSelectedListItem] = useState(null)
 
   const renderListItem = listItem => (
     <ListItem button onClick={() => setSelectedListItem(listItem)}>
-      <ListItemText>{listItem.text}</ListItemText>
+      <ListItemText>{listItem.title}</ListItemText>
     </ListItem>
   )
 
-  const getValue = () =>
-    !_.isNil(selectedListItem) ? selectedListItem.text : ""
+  const getSelectedListItem = () =>
+    _.isNil(selectedListItem)
+      ? {}
+      : _.find(listItems, { id: selectedListItem.id })
 
-  const handleChange = event => {
+  const handleListItemChange = key => event => {
     const updatedSelectedListItem = {
       ...selectedListItem,
-      text: event.target.value
+      [key]: event.target.value
     }
     const updatedSelectedListItems = _.map(listItems, listItem =>
       listItem.id === selectedListItem.id ? updatedSelectedListItem : listItem
     )
     setListItems(updatedSelectedListItems)
-    setSelectedListItem(updatedSelectedListItem)
   }
 
   return (
     <div className="App">
-      <Typography variant="h2" gutterBottom>
-        Digital Crafts TODO List
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
+      <Grid container spacing={4}>
+        <Grid item xs={3}>
           <List>{_.map(listItems, renderListItem)}</List>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={8}>
           <TextField
-            value={getValue()}
+            className="App-title"
+            label="Title"
+            value={getSelectedListItem().title}
+            fullWidth={true}
+            onChange={handleListItemChange("title")}
+          />
+          <TextField
+            label="Content"
+            value={getSelectedListItem().content}
             multiline={true}
             fullWidth={true}
-            onChange={handleChange}
+            onChange={handleListItemChange("content")}
           />
         </Grid>
       </Grid>
     </div>
   )
 }
+
+const buildSampleDocument = () => ({
+  id: _.uniqueId(),
+  title: faker.lorem.words(),
+  content: faker.lorem.paragraphs()
+})
 
 export default App
