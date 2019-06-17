de2b50171cb2c6c5e81c3810aa3e6f169cd02901
Don Schrimsher <don4thirteen@gmail.com>
2019-06-17 11:20:39 -0400

    Show static list items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import Typography from "@material-ui/core/Typography"
+import _ from "lodash"
+
+import "./App.css"
 
 function App() {
+  const listItems = [{ text: "Testing, 1, 2, 3" }, { text: "Testing, 4, 5, 6" }]
+  const renderListItem = listItem => (
+    <ListItem button>
+      <ListItemText>{listItem.text}</ListItemText>
+    </ListItem>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h2">Digital Crafts TODO List</Typography>
+      <List>{_.map(listItems, renderListItem)}</List>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
